f4b4dee7081673973a701c388b35ea46b7322a30
tnhnkzc <tunahankuzucuu@gmail.com>
2022-05-04 16:14:45 +0200

    Added Routes For about and contact pages

--- app.js
+++ app.js
@@ -23,6 +23,16 @@ app.get("/", function (req, res) {
     homeStartingContent: homeStartingContent,
   });
 });
+app.get("/about", function (req, res) {
+  res.render("about", {
+    aboutContent: aboutContent,
+  });
+});
+app.get("/contact", function (req, res) {
+  res.render("contact", {
+    contactContent: contactContent,
+  });
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
