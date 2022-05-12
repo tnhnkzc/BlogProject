9a6578d7fb8bcefaa42958741c21801a7d9d49a0
tnhnkzc <tunahankuzucuu@gmail.com>
2022-05-12 18:03:14 +0200

    Added DB connection and modified the backend accordingly.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+require("dotenv").config();
+var _ = require("lodash");
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent =
@@ -14,24 +15,102 @@ const contactContent =
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//Database connection
+mongoose.connect(process.env.MONGODB_URI);
+
+//MongoDB Post Schema
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const post1 = new Post({
+  title: "Hello!",
+  body: "You can add your post by clicking on top right button called < Publish > ",
+});
+//post1.save();
+const defaultPost = [post1];
+
 app.get("/", function (req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
+  //Fetch items from DB
+  Post.find({}, function (err, foundPosts) {
+    if (foundPosts.length === 0) {
+      Post.insertMany(defaultPost, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Succesfully saved the default items to the DB.");
+        }
+      });
+      res.redirect("/");
+    }
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        posts: foundPosts,
+        homeStartingContent: homeStartingContent,
+      });
+    }
   });
 });
+//Post Specific Pages
+app.get("/posts/:postId", function (req, res) {
+  const desiredPostId = _.capitalize(req.params.postId);
+  const storedPostId = Post.findOne(
+    { _id: desiredPostId },
+    function (err, foundPost) {
+      if (foundPost) {
+        res.render("post", {
+          post: foundPost,
+        });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+// Delete Post
+app.post("/delete", function (req, res) {
+  const deleteItem = req.body.deleteItem;
+  Post.findByIdAndDelete(deleteItem, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+// About Page
 app.get("/about", function (req, res) {
   res.render("about", {
     aboutContent: aboutContent,
   });
 });
+//Contact Page
 app.get("/contact", function (req, res) {
   res.render("contact", {
     contactContent: contactContent,
   });
+});
+//Add Post
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+app.post("/compose", function (req, res) {
+  const postTitle = req.body.postTitle;
+  const postBody = req.body.postBody;
+  const post = new Post({
+    title: postTitle,
+    body: postBody,
+  });
+  post.save();
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
